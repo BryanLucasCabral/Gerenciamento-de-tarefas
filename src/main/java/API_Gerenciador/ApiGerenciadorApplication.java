12279ec07f6e10d70b2ca2a79af4e0fe1b93771f
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-06 20:14:30 -0300

    Projeto Criado

---
+++ src/main/java/API_Gerenciador/ApiGerenciadorApplication.java
@@ -0,0 +1,13 @@
+package API_Gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API_Gerenciador/ApiGerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package API_Gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
